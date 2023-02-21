0f00b7a84b03c0164ae815780fa15199ffa3195e
lenn <lenn.angel@externe.maif.fr>
2023-02-21 10:20:07 +0100

    Empty Project

---
+++ app/src/main/java/lr/iut/androidhandler/MainActivity.kt
@@ -0,0 +1,11 @@
+package lr.iut.androidhandler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
